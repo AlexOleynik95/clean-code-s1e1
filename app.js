d33a841d3970c05d5b5fc43b290374898aaf68f4
Alex Oleynik <Slam1247@gmail.com>
2024-01-09 01:03:08 +0300

    !refactor(basic-3.1-3.8, advanced-2.1): full CSS overhaul
    Put CSS to BEM notation
    Rename meaningless classes
    Remove tag and id selectors (except for syle resetting)
    Fix remaining issues with CSS formatting
    Update JS according to CSS changes

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById('new-task');//Add a new task.
-var addButton=document.getElementsByTagName('button')[0];//first button
-var incompleteTaskHolder=document.getElementById('incompleteTasks');//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById('completed-tasks');//completed-tasks
+var taskInput=document.querySelector('.input_add-item');//Add a new task.
+var addButton=document.querySelector('.button_add');//first button
+var incompleteTaskHolder=document.querySelector('.form__list_tasks-incompleted');//ul of #incompleteTasks
+var completedTasksHolder=document.querySelector('.form__list_tasks-completed');//completed-tasks
 
 
 //New task list item
@@ -32,21 +32,29 @@ var createNewTaskElement=function(taskString){
   var deleteButton=document.createElement('button');//delete button
   var deleteButtonImg=document.createElement('img');//delete button image
 
+  listItem.classList.add('form__item');
+
   label.innerText=taskString;
-  label.className='task';
+  label.classList.add('form__label');
 
   //Each elements, needs appending
   checkBox.type='checkbox';
+  checkBox.classList.add('form__input-checkbox');
+
   editInput.type='text';
-  editInput.className='task';
+  editInput.classList.add('form__input-text');
 
   editButton.innerText='Edit'; //innerText encodes special characters, HTML does not.
   editButton.className='edit';
+  editButton.classList.add('form__button');
+  editButton.classList.add('button_edit');
 
-  deleteButton.className='delete';
-  deleteButtonImg.src='./remove.svg';
+  deleteButton.classList.add('form__button');
+  deleteButton.classList.add('button_delete');
   deleteButton.appendChild(deleteButtonImg);
-
+  
+  deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.classList.add('button__img-delete');
 
   //and appending.
   listItem.appendChild(checkBox);
@@ -82,10 +90,10 @@ var editTask=function(){
 
   var listItem=this.parentNode;
 
-  var editInput=listItem.querySelector('input[type=text]');
-  var label=listItem.querySelector('label');
-  var editBtn=listItem.querySelector('.edit');
-  var containsClass=listItem.classList.contains('editMode');
+  var editInput=listItem.querySelector('.form__input-text');
+  var label=listItem.querySelector('.form__label');
+  var editBtn=listItem.querySelector('.button_edit');
+  var containsClass=listItem.classList.contains('form__item_edit');
   //If class of the parent is .editmode
   if(containsClass){
 
@@ -99,7 +107,7 @@ var editTask=function(){
   }
 
   //toggle .editmode on the parent.
-  listItem.classList.toggle('editMode');
+  listItem.classList.toggle('form__item_edit');
 };
 
 
@@ -155,9 +163,9 @@ addButton.addEventListener('click',ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
   console.log('bind list item events');
 //select ListItems children
-  var checkBox=taskListItem.querySelector('input[type=checkbox]');
-  var editButton=taskListItem.querySelector('button.edit');
-  var deleteButton=taskListItem.querySelector('button.delete');
+  var checkBox=taskListItem.querySelector('.form__input-checkbox');
+  var editButton=taskListItem.querySelector('.button_edit');
+  var deleteButton=taskListItem.querySelector('.button_delete');
 
 
   //Bind editTask to edit button.
